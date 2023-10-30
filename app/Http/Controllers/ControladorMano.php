0c3aebaced6277ab667202876822aa34cfb199d7
Oscar Moreno <oscarmorenoarenas72@gmail.com>
2023-10-30 16:10:08 +0100

    ranking- querys depuradas

--- app/Http/Controllers/ControladorMano.php
+++ app/Http/Controllers/ControladorMano.php
@@ -30,92 +30,129 @@ class ControladorMano extends Controller
 
     function rankingManosGanadoras()
     {
-        $ranking = DB::select("SELECT 'piedra' AS opcion,
+        $ranking = DB::select(" WITH TOTALES AS (
+        SELECT 'piedra' AS opcion,
         SUM( (mano_usuario1 = 'piedra' AND mano_usuario2 = 'tijera')
         OR (mano_usuario1 = 'tijera' AND mano_usuario2 = 'piedra') )
         AS total_ganadas, COUNT(*) AS total_partidas FROM mano
-        
+        where mano_usuario1='piedra' or mano_usuario2='piedra'
         UNION
 
         SELECT 'papel' AS opcion,
          SUM( (mano_usuario1 = 'papel' AND mano_usuario2 = 'piedra')
          OR (mano_usuario1 = 'piedra' AND mano_usuario2 = 'papel') )
           AS total_ganadas, COUNT(*) AS total_partidas FROM mano
-
+          where mano_usuario1='papel' or mano_usuario2='papel'
           UNION
           
            SELECT 'tijera' AS opcion,
            SUM( (mano_usuario1 = 'tijera' AND mano_usuario2 = 'papel')
            OR (mano_usuario1 = 'papel' AND mano_usuario2 = 'tijera') )
-           AS total_ganadas, COUNT(*) AS total_partidas FROM mano ORDER BY total_ganadas DESC;");
+           AS total_ganadas, COUNT(*) AS total_partidas FROM mano
+           where mano_usuario1='tijera' or mano_usuario2='tijera'
+           )
+           SELECT
+            opcion,
+            total_ganadas,
+            total_partidas,
+            (total_ganadas * 100.0) / total_partidas AS porcentaje
+            FROM TOTALES
+            ORDER BY total_ganadas DESC");
 
-           return response()->json(['mensaje'=>$ranking]);
+        return response()->json(['mensaje' => $ranking]);
     }
 
-    function rankingManosPerdedoras(){
-        $ranking = DB::select("SELECT 'piedra' AS opcion,
-        SUM( (mano_usuario1 = 'piedra' AND mano_usuario2 = 'papel')
-        OR (mano_usuario1 = 'papel' AND mano_usuario2 = 'piedra') )
-        AS total_ganadas, COUNT(*) AS total_partidas FROM manos
-        
-        UNION 
-
-        SELECT 'papel' AS opcion,
-         SUM( (mano_usuario1 = 'papel' AND mano_usuario2 = 'tijera')
-         OR (mano_usuario1 = 'tijera' AND mano_usuario2 = 'papel') )
-          AS total_ganadas, COUNT(*) AS total_partidas FROM manos
-
-          UNION
+    function rankingManosPerdedoras()
+    {
+        $ranking = DB::select("WITH Totales AS (
+            SELECT
+              'piedra' AS opcion,
+              SUM((mano_usuario1 = 'piedra' AND mano_usuario2 = 'papel') OR (mano_usuario1 = 'papel' AND mano_usuario2 = 'piedra')) AS total_perdidas,
+              COUNT(*) AS total_partidas
+            FROM mano
+            WHERE mano_usuario1 = 'piedra' OR mano_usuario2 = 'piedra'
+            UNION
+            SELECT
+              'papel' AS opcion,
+              SUM((mano_usuario1 = 'papel' AND mano_usuario2 = 'tijera') OR (mano_usuario1 = 'tijera' AND mano_usuario2 = 'papel')) AS total_perdidas,
+              COUNT(*) AS total_partidas
+            FROM mano
+            WHERE mano_usuario1 = 'papel' OR mano_usuario2 = 'papel'
+            UNION
+            SELECT
+              'tijera' AS opcion,
+              SUM((mano_usuario1 = 'tijera' AND mano_usuario2 = 'piedra') OR (mano_usuario1 = 'piedra' AND mano_usuario2 = 'tijera')) AS total_perdidas,
+              COUNT(*) AS total_partidas
+            FROM mano
+            WHERE mano_usuario1 = 'tijera' OR mano_usuario2 = 'tijera'
+          )
           
-           SELECT 'tijera' AS opcion,
-           SUM( (mano_usuario1 = 'tijera' AND mano_usuario2 = 'piedra')
-           OR (mano_usuario1 = 'piedra' AND mano_usuario2 = 'tijera') )
-           AS total_ganadas, COUNT(*) AS total_partidas FROM manos ORDER BY opcion;");
+          SELECT
+            opcion,
+            total_perdidas,
+            total_partidas,
+            (total_perdidas * 100.0) / total_partidas AS porcentaje
+          FROM Totales
+          ORDER BY total_perdidas DESC;
+          ");
 
-           return response()->json(['mensaje'=>$ranking]);
-
+        return response()->json(['mensaje' => $ranking]);
     }
-    function rankingManosNulas(){
-        $ranking = DB::select("SELECT 'piedra' AS opcion,
+    function rankingManosNulas()
+    {
+        $ranking = DB::select(" WITH TOTALES AS (SELECT 'piedra' AS opcion,
         SUM( (mano_usuario1 = 'piedra' AND mano_usuario2 = 'piedra') )
-        AS total_ganadas, COUNT(*) AS total_partidas FROM manos
-        
+        AS total_empatadas, COUNT(*) AS total_partidas FROM mano
+        WHERE mano_usuario1 = 'piedra' OR mano_usuario2 = 'piedra'
         UNION
 
         SELECT 'papel' AS opcion,
          SUM( (mano_usuario1 = 'papel' AND mano_usuario2 = 'papel')  )
-          AS total_ganadas, COUNT(*) AS total_partidas FROM manos
-
+          AS total_empatadas, COUNT(*) AS total_partidas FROM mano
+          WHERE mano_usuario1 = 'papel' OR mano_usuario2 = 'papel'
           UNION
           
            SELECT 'tijera' AS opcion,
            SUM( (mano_usuario1 = 'tijera' AND mano_usuario2 = 'tijera') )
-           AS empates, COUNT(*) AS total_partidas FROM manos ORDER BY opcion;");
+           AS total_empatadas, COUNT(*) AS total_partidas FROM mano 
+           WHERE mano_usuario1 = 'tijera' OR mano_usuario2 = 'tijera'
+           )
+           SELECT
+            opcion,
+            total_empatadas,
+            total_partidas,
+            (total_empatadas * 100.0) / total_partidas AS porcentaje
+          FROM TOTALES
+          ORDER BY total_empatadas DESC;");
 
-           return response()->json(['mensaje'=>$ranking]);
-
+        return response()->json(['mensaje' => $ranking]);
     }
-    function rankingManosMasJugadas(){
-        $ranking = DB::select("SELECT 'piedra' AS opcion, 
-        SUM( (mano_usuario1 = 'piedra' AND mano_usuario2 = 'piedra') ) 
-        AS total_ganadas, COUNT(*) AS total_partidas FROM manos 
-        
-        UNION 
+    function rankingManosMasJugadas()
+    {
+        $ranking = DB::select(" WITH TOTALES AS (SELECT 'piedra' AS opcion,
+        SUM( (mano_usuario1 = 'piedra' or mano_usuario2 = 'piedra') )
+        AS total_jugadas, COUNT(*) AS total_partidas FROM mano
+     
+        UNION
 
         SELECT 'papel' AS opcion,
-         SUM( (mano_usuario1 = 'papel' AND mano_usuario2 = 'papel')  )
-          AS total_ganadas, COUNT(*) AS total_partidas FROM manos 
-
+         SUM( (mano_usuario1 = 'papel' or mano_usuario2 = 'papel')  )
+          AS total_jugadas, COUNT(*) AS total_partidas FROM mano
+         
           UNION
           
            SELECT 'tijera' AS opcion,
-           SUM( (mano_usuario1 = 'tijera' AND mano_usuario2 = 'tijera') )
-           AS empates, COUNT(*) AS total_partidas FROM manos ORDER BY opcion;");
+           SUM( (mano_usuario1 = 'tijera' or mano_usuario2 = 'tijera') )
+           AS total_jugadas, COUNT(*) AS total_partidas FROM mano 
+           )
+           SELECT
+            opcion,
+            total_jugadas,
+            total_partidas,
+            (total_jugadas * 100.0) / total_partidas AS porcentaje
+          FROM TOTALES
+          ORDER BY total_jugadas DESC;");
 
-           return response()->json(['mensaje'=>$ranking]);
-
+        return response()->json(['mensaje' => $ranking]);
     }
-
-
-
 }
